f5e49b6a8ead9aa7ac704dab338279ca8289c2b3
connor-kfitz <connorkfitzsimmons@gmail.com>
2022-05-25 20:47:21 -0400

    Added Colour Feature based on time

--- script.js
+++ script.js
@@ -1,5 +1,26 @@
 $('#currentDate').text(moment().format('MMM Do, YYYY'));
 
-var currentTime = moment().format('h');
-console.log(currentTime);
+var currentTime = 12;
+// moment().format('h');
+
+var timeBlock = [9,10,11,12,1,2,3,4,5];
+var timeBlockString = ['#9','#10','#11','#12','#1','#2','#3','#4','#5'];
+
+console.log(timeBlockString);
+
+for(var i=0; i < timeBlock.length; i++){
+    if (currentTime == timeBlock[i]){
+        var currentTimeBlock = $(timeBlockString[i]);
+        currentTimeBlock.css("background-color", "yellow");
+
+        for(var j=0; j < i; j++){
+            var currentTimeBlock = $(timeBlockString[j]);
+            currentTimeBlock.css("background-color", "blue");
+        }
+        for(var j=timeBlockString.length; j > i; j--){
+            var currentTimeBlock = $(timeBlockString[j]);
+            currentTimeBlock.css("background-color", "red");
+        }
+    }
+}
 
